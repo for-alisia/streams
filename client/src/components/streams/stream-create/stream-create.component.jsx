7b958f9f96076688a86155ffaf937159779f3039
for-alisia <for-alisia@yandex.ru>
2021-02-20 11:07:42 +0300

    Added json-server for dev-backend

--- client/src/components/streams/stream-create/stream-create.component.jsx
+++ client/src/components/streams/stream-create/stream-create.component.jsx
@@ -11,8 +11,8 @@ const validate = (formValues) => {
   return errors;
 };
 
-class StreamCreate extends React.Component {
-  renderError = ({ error, touched }) => {
+const StreamCreate = ({ handleSubmit }) => {
+  const renderError = ({ error, touched }) => {
     if (error && touched)
       return (
         <div className="ui error message">
@@ -21,31 +21,66 @@ class StreamCreate extends React.Component {
       );
   };
 
-  renderInput = ({ input, label, meta }) => {
+  const renderInput = ({ input, label, meta }) => {
     return (
       <div className={`field ${meta.error && meta.touched ? 'error' : null}`}>
         <label>{label}</label>
         <input {...input} autoComplete="off" />
-        <div>{this.renderError(meta)}</div>
+        <div>{renderError(meta)}</div>
       </div>
     );
   };
 
-  onSubmit = (formValues) => {
+  const onSubmit = (formValues) => {
     console.log(formValues);
   };
 
-  render() {
-    return (
-      <div>
-        <form className="ui form error" onSubmit={this.props.handleSubmit(this.onSubmit)}>
-          <Field name="title" component={this.renderInput} label="Enter title" />
-          <Field name="description" component={this.renderInput} label="Enter description" />
-          <button className="ui button primary">Submit</button>
-        </form>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <form className="ui form error" onSubmit={handleSubmit(onSubmit)}>
+        <Field name="title" component={renderInput} label="Enter title" />
+        <Field name="description" component={renderInput} label="Enter description" />
+        <button className="ui button primary">Submit</button>
+      </form>
+    </div>
+  );
+};
 
 export default reduxForm({ form: 'createStream', validate })(StreamCreate);
+
+// class _StreamCreate extends React.Component {
+//   renderError = ({ error, touched }) => {
+//     if (error && touched)
+//       return (
+//         <div className="ui error message">
+//           <div className="header">{error}</div>
+//         </div>
+//       );
+//   };
+
+//   renderInput = ({ input, label, meta }) => {
+//     return (
+//       <div className={`field ${meta.error && meta.touched ? 'error' : null}`}>
+//         <label>{label}</label>
+//         <input {...input} autoComplete="off" />
+//         <div>{this.renderError(meta)}</div>
+//       </div>
+//     );
+//   };
+
+//   onSubmit = (formValues) => {
+//     console.log(formValues);
+//   };
+
+//   render() {
+//     return (
+//       <div>
+//         <form className="ui form error" onSubmit={this.props.handleSubmit(this.onSubmit)}>
+//           <Field name="title" component={this.renderInput} label="Enter title" />
+//           <Field name="description" component={this.renderInput} label="Enter description" />
+//           <button className="ui button primary">Submit</button>
+//         </form>
+//       </div>
+//     );
+//   }
+// }
