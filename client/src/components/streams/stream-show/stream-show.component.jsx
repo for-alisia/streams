b05a98b61c951bfb43ad2fba41895dc67e735862
for-alisia <for-alisia@yandex.ru>
2021-02-21 11:14:01 +0300

    Added streaming server and connected to a frontend

--- client/src/components/streams/stream-show/stream-show.component.jsx
+++ client/src/components/streams/stream-show/stream-show.component.jsx
@@ -1,16 +1,35 @@
+// @ts-nocheck
 /** Libraries */
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { connect } from 'react-redux';
+import flv from 'flv.js';
 
 /** Redux */
 import { fetchStream } from '../../../actions';
 
 const StreamShow = ({ fetchStream, match, stream }) => {
+  const videoRef = useRef();
   useEffect(() => {
     fetchStream(match.params.id);
   }, [fetchStream, match]);
+  useEffect(() => {
+    if (stream) {
+      const player = flv.createPlayer({
+        type: 'flv',
+        url: `http://localhost:8000/live/${stream.id}.flv`,
+      });
+
+      player.attachMediaElement(videoRef.current);
+      player.load();
+
+      return () => {
+        player.destroy();
+      };
+    }
+  }, [stream]);
   return (
     <div>
+      <video ref={videoRef} style={{ width: '100%' }} controls />
       {stream ? (
         <div>
           <h1>{stream.title}</h1>
